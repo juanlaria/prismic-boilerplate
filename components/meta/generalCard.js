7caa35b8c3f9e159f58cdb8418f614dc3ac9fc61
Juan Laria <juan.laria@aerolab.co>
2020-05-09 01:19:20 -0300

    Public name env variable added

--- components/meta/generalCard.js
+++ components/meta/generalCard.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+const { PUBLIC_NAME } = process.env;
+
 export default function GeneralCard({ canonical, title, description, image }) {
   return (
     <Head>
@@ -10,7 +12,7 @@ export default function GeneralCard({ canonical, title, description, image }) {
       {description && <meta name="og:description" content={description} />}
       {image && <meta name="og:image" content={image.url} />}
       <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="Site Name" />
+      <meta property="og:site_name" content={PUBLIC_NAME} />
     </Head>
   );
 }
--- components/meta/metadata.js
+++ components/meta/metadata.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+const { PUBLIC_NAME } = process.env;
+
 export default function Metadata({
   title,
   canonical,
@@ -9,14 +11,10 @@ export default function Metadata({
 }) {
   return (
     <Head>
-      <title>{title || 'Site Name'}</title>
+      <title>{title || PUBLIC_NAME}</title>
       {canonical && <link rel="canonical" href={canonical} />}
-      {description && (
-        <meta name="description" content={description} />
-      )}
-      {keywords && (
-        <meta name="keywords" content={keywords} />
-      )}
+      {description && <meta name="description" content={description} />}
+      {keywords && <meta name="keywords" content={keywords} />}
       {indexing && <meta name="robots" content={indexing} />}
     </Head>
   );
