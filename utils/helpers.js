17b1faf6243b5360c7639251974aded8e5f0fcde
Juan Laria <juan.laria@aerolab.co>
2020-05-25 16:02:39 -0300

    New helper functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,16 @@
 export const objectNotEmpty = obj => {
   return !(Object.keys(obj).length === 0 && obj.constructor === Object);
 };
+
+export const isBrowser =
+  typeof window !== 'undefined' && typeof window.document !== 'undefined';
+
+export const isWebWorker =
+  typeof self === 'object' &&
+  self.constructor &&
+  self.constructor.name === 'DedicatedWorkerGlobalScope';
+
+export const isNode =
+  typeof process !== 'undefined' &&
+  process.versions != null &&
+  process.versions.node != null;
