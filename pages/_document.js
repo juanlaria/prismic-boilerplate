dbf1e1ab1fae184f8106a09f78740b11454cc39c
Juan Laria <juan.laria@aerolab.co>
2020-05-25 16:50:57 -0300

    PWA

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import { extractCritical } from 'emotion-server';
 
+const { PUBLIC_NAME } = process.env;
+
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const page = renderPage();
@@ -16,7 +18,23 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html lang={this.lang}>
+      <html lang={this.lang} dir='ltr'>
+        <meta name="application-name" content={PUBLIC_NAME} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={PUBLIC_NAME} />
+        <meta name="description" content="Example site description" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#FFFFFF" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/180x180.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <Head>
           <style
             data-emotion-css={this.props.ids.join(' ')}
