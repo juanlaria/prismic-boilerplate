ebb5fa74fdb1796b648047f1aa6853fc5a92486f
Juan Laria <juan.laria@aerolab.co>
2020-05-24 23:55:21 -0300

    CMS - Import footer component

--- components/cms/index.js
+++ components/cms/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable import/no-cycle */
 import { default as hero } from './hero';
 import { default as banner_with_caption } from './banner';
+import { default as footer } from './footer';
 
 export default {
   hero,
   banner_with_caption,
+  footer,
 };
