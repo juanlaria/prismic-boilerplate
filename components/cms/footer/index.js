60032f908a5c1d306be181e27a48e409fbdb2a39
Juan Laria <juan.laria@aerolab.co>
2020-05-24 23:56:49 -0300

    Footer - Show data

--- components/cms/footer/index.js
+++ components/cms/footer/index.js
@@ -1,11 +1,29 @@
+import { RichText } from 'prismic-reactjs';
 import FooterSection from './styles';
+import Link from '../../link';
 
-const Footer = () => {
-  return (
-    <FooterSection>
-      <h1>Footer</h1>
-    </FooterSection>
-  );
+const Footer = ({ data }) => {
+  if (data) {
+    const { footer_links, footer_icon, footer_text } = data;
+    return (
+      <FooterSection>
+        {!!footer_links.length && (
+          <ul>
+            {footer_links.map(el => (
+              <li>
+                <Link link={el.link}>
+                  <a>{RichText.asText(el.link_label)}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+        <hr />
+        {footer_text && <RichText render={footer_text} />}
+      </FooterSection>
+    );
+  }
+  return false;
 };
 
 export default Footer;
