17a86c07fb280605bc07644e66e161b31fd4f61d
Juan Laria <juan.laria@aerolab.co>
2020-05-24 23:56:16 -0300

    Doc page

---
+++ pages/doc/[id].js
@@ -0,0 +1,27 @@
+import { getAllDocsIds, getDocData } from '../../utils/api';
+import Render from '../../components/render';
+
+export default ({ doc, preview }) => {
+  return <Render data={doc} />
+};
+
+export async function getStaticPaths() {
+  // Return a list of possible value for ID
+  const paths = await getAllDocsIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ req, params, preview, previewData }) {
+  //Return page data
+  const { id } = params;
+  const doc = await getDocData(req, id, previewData);
+  return {
+    props: {
+      preview: !!preview,
+      doc,
+    },
+  };
+}
