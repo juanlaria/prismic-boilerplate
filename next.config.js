81edd47716aa7cfbd3f43954a7a3880283a5f102
Juan Laria <juan.laria@aerolab.co>
2020-07-09 21:51:41 -0300

    Delete Next config

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withPWA = require('next-pwa');
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = withPWA({
-  pwa: {
-    // disable: !isProd,
-    dest: 'public',
-  },
-});
